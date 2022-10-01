5da799dc1344d3b03ab802ee90426a7b8f3380f5
luciadeizaguirre <luciadeizaguirre@gmail.com>
2022-10-01 15:14:10 +0200

    otros

---
+++ ejercicio6.py
@@ -0,0 +1,20 @@
+lista=list(1,2,13,23,3,26,6)
+lista_pares=[]
+lista_impares=[]
+def separar():
+    for i in lista:        
+        if i % 2 == 0:
+            lista_pares.append()
+            lista_pares.sort()
+        else:
+            lista_impares.append(str(i))
+            lista_impares.sort()
+    
+print(lista_pares)
+print(lista_impares)
+elementos=input("Introduzca 5 caracteres:  ")
+lista=list(elementos)
+print(lista)
+def agregar_una_vez(lista):
+    elementos_nuevo=input("Escriba uno o varios elementos para agregar a la lista: ")
+    for elementos in lista
